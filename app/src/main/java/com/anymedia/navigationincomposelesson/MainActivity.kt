773a8d9fb43340e6b6ac40b93b8d878a5aa3aa17
Elmahdaoui <abdelghani.elmahdaoui@adria-bt.com>
2024-08-16 16:51:20 +0100

    Added floating action button

--- app/src/main/java/com/anymedia/navigationincomposelesson/MainActivity.kt
+++ app/src/main/java/com/anymedia/navigationincomposelesson/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
@@ -32,6 +34,8 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -48,6 +52,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
@@ -116,7 +121,9 @@ fun MyApp() {
         },
         bottomBar = {
             val currentRoute = navController.currentBackStackEntry?.destination?.route
-            BottomNavigation {
+            BottomNavigation(
+                backgroundColor = MaterialTheme.colorScheme.primaryContainer
+            ) {
                 BottomNavigationItem(
                     icon = { Icon(imageVector = Icons.Default.Home, contentDescription = null) },
                     label = { Text(text = "Main") },
@@ -136,7 +143,17 @@ fun MyApp() {
                     selected = currentRoute == Screen.MainScreen.route,
                     onClick = { navController.navigate(Screen.SettingScreen.route) }
                 )
-
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                    shape = CircleShape
+                    ) {
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                    }
+                }
             }
         }
     ) { padding ->
